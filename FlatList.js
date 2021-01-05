1445690765ba80f085857d6071625f7aca4899b0
Elijah Silang <iv.elijah.s@gmail.com>
2021-01-05 16:04:22 -0600

    UI Improvements

--- FlatList.js
+++ FlatList.js
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, FlatList, StyleSheet, View } from 'react-native';
+import { Text, FlatList, StyleSheet, View, Image } from 'react-native';
 
 const Flist = () => {
     //data source
     const people = [
-        { name: 'elijah', key: 1},
-        { name: 'kevin', key: 2},
-        { name: 'krunal', key: 3},
-        { name: 'sebastian', key: 4},
-        { name: 'zhiwei', key: 5},
-        { name: 'aida', key: 6},
+        { name: 'elijah', message: 'Love For All, Hatred For None', img_url: 'https://picsum.photos/200', key: 1},
+        { name: 'kevin', message: 'Change the world by being yourself', img_url: 'https://picsum.photos/200', key: 2},
+        { name: 'krunal', message: 'Every moment is a fresh beginning', img_url: 'https://picsum.photos/200', key: 3},
+        { name: 'sebastian', message: 'Aspire to inspire before we expire', img_url: 'https://picsum.photos/200', key: 4},
+        { name: 'zhiwei', message: 'Everything you can imagine is real', img_url: 'https://picsum.photos/200', key: 5},
+        { name: 'aida', message: 'Whatever you do, do it well', img_url: 'https://picsum.photos/200', key: 6},
     ];
     const friendsArray = [
         {name : "Krunal", key:1},
@@ -25,11 +25,21 @@ const Flist = () => {
       ];
     return(
         <View style={styles.container}>
-
             <FlatList
-                data = {friendsArray}
+                data = {people}
                 renderItem = {({ item }) => (
-                    <Text>{item.name}</Text>
+                    <View style={styles.coloredBox}>
+                        <Image
+                            style = {styles.icon}
+                            source = {{uri: item.img_url}}
+                            //source = {require('./assets/icon.png')}
+                        />
+                        
+                        <View>
+                            <Text style={styles.name}>{item.name}</Text>
+                            <Text style={styles.message}>{item.message}</Text>
+                        </View>
+                    </View>
                 )}
             />
         </View>
@@ -49,9 +59,26 @@ const styles = StyleSheet.create({
     coloredBox: {
         marginTop: 24,
         padding: 20,
+        flexDirection: 'row',
         fontSize: 24,
         fontWeight: 'bold',
-        backgroundColor: 'teal',
+        borderColor: 'gray',
+        borderRadius: 50    ,
+        borderWidth: 2,
         fontSize: 24,
+    },
+    name: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    message: {
+        color: 'gray',
+        fontStyle: 'italic',
+    },
+    icon: {
+        width: 50,
+        height: 50,
+        borderRadius: 40,
+        marginRight: 10,
     },
 });
